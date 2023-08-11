3ea0054f3f415dbf6cad98550b3da31347760d8d
JuanVictorDavila <juanvictor70@gmail.com>
2023-08-11 16:01:03 -0400

    feat: implementing the tests for product manufacturer field

--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -20,10 +20,10 @@ module.exports = Joi.object({
     'any.required': '"category" is required',
   }),
   manufacturer: Joi.string().min(2).required({
-    'string.min': '"name" length must be at least 3 characters long',
-    'string.base': '"name" should be a string',
-    'string.empty': '"name" must contain value',
-    'any.required': '"name" is required',
+    'string.min': '"manufacturer" length must be at least 3 characters long',
+    'string.base': '"manufacturer" should be a string',
+    'string.empty': '"manufacturer" must contain value',
+    'any.required': '"manufacturer" is required',
   }),
   price: Joi.number().min(1).required().messages({
     'number.min': '"price" must be a number larger than or equal to 1',
